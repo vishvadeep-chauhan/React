6fc22087bb0d92ec59bbcee3ae2e4f43a40e05e8
Vishvadeep Singh Chauhan <vishvadeepsinghchauhan@gmail.com>
2025-01-28 00:52:32 +0530

    react-aaps

---
+++ my-vitereact-app/src/App.jsx
@@ -0,0 +1,14 @@
+import Vishv from "./vishv";
+
+function App() {
+  return (
+    <>
+      <Vishv />
+      <p>My name is Vishv</p>
+
+      <h1>hello</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ my-vitereact-app/src/vishv.jsx
@@ -0,0 +1,9 @@
+function Vishv() {
+  return (
+    <div>
+      <h1>My name is Vishv | vite</h1>
+    </div>
+  );
+}
+
+export default Vishv;
